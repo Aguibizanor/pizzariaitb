26b116d7abac7e79313170ed48077e1dc7802e0a
aguibizanor@gmail.com <aguibizanor@gmail.com>
2024-07-29 08:29:01 -0300

    imagens e outros

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import Listagem from "./pages/Listagem";
 
 const App = () => {
+
+  const [color] = useState("#0000FF")
+  document.body.style.backgroundColor = color
   
   return (
     <Routes>
-      <Route path="/" />
+      <Route path="/" element={<Listagem />} />
     </Routes>
   )
 }
